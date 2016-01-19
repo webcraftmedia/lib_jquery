5cbffcf4e9a888ee0a80addaf2a68006cc8a7124
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-19 21:51:09 +0100

    fixed path

--- lib_jquery.php
+++ lib_jquery.php
@@ -4,7 +4,7 @@ class lib_jquery extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/jquery/lib/jquery-2.1.4.min.js');}
+        return new \SYSTEM\PLIB('/jquery/lib/jquery-2.1.4.min.js');}
     public static function version(){
         return '2.1.4';}
 }
