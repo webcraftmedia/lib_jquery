edc616c1753577158eada5283c977f1346abf7ec
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-02-19 15:40:35 +0100

    updated jquery to v 3.3.1

--- lib_jquery_2.1.4.php
+++ lib_jquery_2.1.4.php
@@ -1,10 +1,10 @@
 <?php
 namespace LIB;
-class lib_jquery extends \LIB\lib_js{
+class lib_jquery_2_1_4 extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return new \SYSTEM\PLIB('/jquery/lib/jquery-3.3.1.min.js');}
+        return new \SYSTEM\PLIB('/jquery/lib/jquery-2.1.4.min.js');}
     public static function version(){
-        return '3.3.1';}
+        return '2.1.4';}
 }
