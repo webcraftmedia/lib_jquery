b6c56b2d669e1161b89d9d6a58f207e4f75848ea
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-08-02 17:52:06 +0200

    corrected path

--- lib_jquery.php
+++ lib_jquery.php
@@ -4,7 +4,7 @@ class lib_jquery extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return new \SYSTEM\PLIB('/jquery/lib/jquery-3.3.1.min.js');}
+        return new \SYSTEM\PLIB('jquery/lib/jquery-3.3.1.min.js');}
     public static function version(){
         return '3.3.1';}
 }
